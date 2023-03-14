d5476e795668d82347c9c464be4653d799c12a85
alimohdapg <alimohdapg@gmail.com>
2023-03-14 00:43:59 +0000

    added index page redirection

--- webapps2023/settings.py
+++ webapps2023/settings.py
@@ -129,10 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
--- webapps2023/urls.py
+++ webapps2023/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from payapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include('register.urls')),
-    path('payapp/', include('payapp.urls'))
+    path('payapp/', include('payapp.urls')),
+    path('', views.home, name='index')
 ]
