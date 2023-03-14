c1825471826e20f9df15694c5769424f82540bd8
alimohdapg <alimohdapg@gmail.com>
2023-03-14 01:15:06 +0000

    added whitenoise middleware

--- webapps2023/settings.py
+++ webapps2023/settings.py
@@ -61,6 +61,8 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 ROOT_URLCONF = 'webapps2023.urls'
 
 TEMPLATES = [
